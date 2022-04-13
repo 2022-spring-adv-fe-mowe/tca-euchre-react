6769928a6c7323327b34d708fd89d7e154b818ee
rissba05 <rissba05@gmail.com>
2022-04-13 15:42:44 -0500

    started to try to navigate to setup screen but not working yet...

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
+import { Home } from './Home';
 
-const Home = () => <h2>Home</h2>
 const GameSetup = () => <h2>Game Setup</h2>
 const PlayGame = () => <h2>Play Game</h2>
 
 function App() {
   return (
     <div className="App">
-
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="setup" element={<GameSetup />} />
-      <Route path="play" element={<PlayGame />} />
-    </Routes>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="setup" element={<GameSetup />} />
+        <Route path="play" element={<PlayGame />} />
+      </Routes>
     </div>
   );
 }
