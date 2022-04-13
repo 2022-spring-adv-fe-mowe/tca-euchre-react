4ab534703199d48bc61a725c4b6843ff0b5087af
rissba05 <rissba05@gmail.com>
2022-04-13 15:01:56 -0500

    dummy components per screen...
    for now...

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import { Button } from '@mui/material';
 
+const Home = () => <h2>Home</h2>
+const GameSetup = () => <h2>Game Setup</h2>
+const PlayGame = () => <h2>Play Game</h2>
+
 function App() {
   return (
     <div className="App">
       <Button variant="outlined">
         Play
       </Button>
+      <Home />
+      <GameSetup />
+      <PlayGame />
     </div>
   );
 }
